228182ca44846123223b39dbeb7cde632c2f00ee
Tina Shan <tinashan22@gmail.com>
2021-04-03 15:45:24 +0800

    menu page

---
+++ pages/menu/menu.js
@@ -0,0 +1,53 @@
+// pages/menu/menu.js
+const app = getApp()
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+   meals:[],
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad() {
+    const self = this
+    let Menu = new wx.BaaS.TableObject('hackathon_meals')
+    console.log(Menu)
+    Menu.find().then(
+      (res) => {
+        console.log('res', res)
+        self.setData({
+          meals: res.data.objects
+        })
+      }, (err) => {
+          console.log("err", err)
+      }
+    );
+
+    console.log(self.data.meals)
+
+  // createOrder: function(e){
+  //   console.log("orders", e)
+  //   let order = new wx.BaaS.TableObject('hackathon_orders')
+  //   let newOrder = order.create()
+  //   newOrder.set({
+  //     meal_id: e.currentTarget.dataset.meal_id,
+  //     count: 1
+  //   })
+
+  //   newOrder.save().then((res)=>{
+  //     wx.showToast({
+  //       title: 'Ordered!',
+  //     })
+  //   }),
+  //   console.log("afterOrder", this.data.meals)
+  // }
+
+
+  }
+})
