db2889dbee2a3592572996464f592e3da20f2f6f
Tina Shan <tinashan22@gmail.com>
2021-04-03 14:26:58 +0800

    initialized the MP with ifanr

---
+++ app.js
@@ -0,0 +1,24 @@
+// app.js
+App({
+  onLaunch() {
+    wx.BaaS = requirePlugin('sdkPlugin')
+    //让插件帮助完成登录、支付等功能
+    wx.BaaS.wxExtend(wx.login,
+     wx.getUserInfo,
+     wx.requestPayment)
+
+    wx.BaaS.init('a3fb2113c0a97830d15b')
+    const self = this
+    wx.BaaS.auth.getCurrentUser().then((res)=>{
+      wx.setStorageSync('userInfo', res)
+      self.globalData.userInfo = res
+      },(err)=>{console.log('err', err)}
+    )
+
+  
+  },
+
+  globalData: {
+    userInfo: wx.getStorageSync('userInfo')
+  }
+})
